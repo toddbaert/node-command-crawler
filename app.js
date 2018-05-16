b71f56f56fbc577632213185d4f76754f4491276
Baert <todd.baert@tag009442170705.dynatrace.org>
2018-05-16 14:36:18 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var chokidar = require("chokidar");
+var child = require("child_process");
+var targetDirectory = process.argv[2];
+var commandTemplate = process.argv[3];
+var startupTime = new Date().getTime();
+var processed = [];
+console.log('Starting node command crawler on: ' + targetDirectory);
+chokidar.watch(targetDirectory, { ignored: /(^|[\/\\])\../ }).on('add', function (path) {
+    if (fs.statSync(path).ctimeMs > startupTime && processed.indexOf(path) < 0) {
+        console.log('Found file: ' + path);
+        child.exec('ffmpeg %file% -c:v copy -c:a aac -vbr 3 .%file%.processing'.replace('%file%', path));
+        child.exec('mv .%file%.processing %file%'.replace('%file%', path));
+        processed.push(path);
+    }
+});
---
+++ app.ts
@@ -0,0 +1,18 @@
+import fs = require('fs');
+import chokidar = require('chokidar');
+import child = require('child_process');
+
+const targetDirectory : string = process.argv[2];
+const commandTemplate : string = process.argv[3];
+const startupTime: number = new Date().getTime();
+const processed: string[] = [];
+
+console.log('Starting node command crawler on: ' + targetDirectory);
+chokidar.watch(targetDirectory, {ignored: /(^|[\/\\])\../}).on('add', (path) => {
+    if (fs.statSync(path).ctimeMs > startupTime && processed.indexOf(path) < 0) {
+        console.log('Found file: ' + path);
+        child.exec('ffmpeg %file% -c:v copy -c:a aac -vbr 3 .%file%.processing'.replace('%file%', path));
+        child.exec('mv .%file%.processing %file%'.replace('%file%', path));
+        processed.push(path);
+    }
+});
