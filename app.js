45d808d7cc8b87306c8679084d25ac8aaac5e75c
Baert <todd.baert@tag009442170705.dynatrace.org>
2018-05-16 16:36:47 -0400

    refactoring to be called by nzb

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
-var chokidar = require("chokidar");
 var child = require("child_process");
 var targetDirectory = process.argv[2];
-var commandTemplate = process.argv[3];
-var startupTime = new Date().getTime();
-var processed = [];
-console.log('Starting node command crawler on: ' + targetDirectory);
-chokidar.watch(targetDirectory, {
-    ignored: /(^|[\/\\])\../,
-    awaitWriteFinish: {
-        stabilityThreshold: 3000,
-        pollInterval: 250
-    }
-}).on('add', function (path) {
-    if (fs.statSync(path).ctimeMs > startupTime && processed.indexOf(path) < 0) {
-        console.log('Found file: ' + path);
-        var tempPath = path.replace(/(\.[\w\d_-]+)$/i, '_temp$1');
-        child.execSync(('ffmpeg -i %file% -c:v copy -c:a aac -vbr 3 ' + tempPath).replace(/%file%/g, path));
-        child.execSync(('mv %file%.processing ' + tempPath).replace(/%file%/g, path));
-        processed.push(path);
-        console.log('Finished processing file: ' + path);
-    }
+var files = fs.readdirSync(targetDirectory);
+files.forEach(function (file) {
+    var tempFile = file.replace(/(\.[\w\d_-]+)$/i, '_temp$1');
+    child.execSync(('ffmpeg -i %file% -c:v copy -c:a aac -vbr 3 ' + tempFile).replace(/%file%/g, file));
+    child.execSync(('mv %file%.processing ' + tempFile).replace(/%file%/g, file));
 });
-function execCallback(error, stdout, stderr) {
-    console.log(error);
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-}
--- app.ts
+++ app.ts
@@ -3,30 +3,11 @@ import chokidar = require('chokidar');
 import child = require('child_process');
 
 const targetDirectory : string = process.argv[2];
-const commandTemplate : string = process.argv[3];
-const startupTime: number = new Date().getTime();
-const processed: string[] = [];
 
-console.log('Starting node command crawler on: ' + targetDirectory);
-chokidar.watch(targetDirectory, {
-    ignored: /(^|[\/\\])\../,
-        awaitWriteFinish: {
-            stabilityThreshold: 3000,
-            pollInterval: 250
-        }
-    }).on('add', (path) => {
-    if (fs.statSync(path).ctimeMs > startupTime && processed.indexOf(path) < 0) {
-        console.log('Found file: ' + path);
-        var tempPath: string = path.replace(/(\.[\w\d_-]+)$/i, '_temp$1');
-        child.execSync(('ffmpeg -i %file% -c:v copy -c:a aac -vbr 3 ' + tempPath).replace(/%file%/g, path));
-        child.execSync(('mv %file%.processing ' + tempPath).replace(/%file%/g, path));
-        processed.push(path);
-        console.log('Finished processing file: ' + path);
-    }
+var files: string[] = fs.readdirSync(targetDirectory);
+files.forEach(file => {
+    var tempFile: string = file.replace(/(\.[\w\d_-]+)$/i, '_temp$1');
+    child.execSync(('ffmpeg -i %file% -c:v copy -c:a aac -vbr 3 ' + tempFile).replace(/%file%/g, file));
+    child.execSync(('mv %file%.processing ' + tempFile).replace(/%file%/g, file));
 });
 
-function execCallback(error: Error | null, stdout: string, stderr: string) {
-    console.log(error);
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-}
