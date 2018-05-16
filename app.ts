e5b65a51d17d46fd7012ec7ecce8caf2bb761250
Baert <todd.baert@tag009442170705.dynatrace.org>
2018-05-16 14:56:58 -0400

    fixing path

--- app.ts
+++ app.ts
@@ -11,8 +11,9 @@ console.log('Starting node command crawler on: ' + targetDirectory);
 chokidar.watch(targetDirectory, {ignored: /(^|[\/\\])\../}).on('add', (path) => {
     if (fs.statSync(path).ctimeMs > startupTime && processed.indexOf(path) < 0) {
         console.log('Found file: ' + path);
-        child.exec('ffmpeg %file% -c:v copy -c:a aac -vbr 3 .%file%.processing'.replace('%file%', path));
-        child.exec('mv .%file%.processing %file%'.replace('%file%', path));
+        child.exec('ffmpeg %file% -c:v copy -c:a aac -vbr 3 %file%.processing'.replace('%file%', path));
+        child.exec('mv %file%.processing %file%'.replace('%file%', path));
         processed.push(path);
+        console.log('Finished processing file: ' + path);
     }
 });
