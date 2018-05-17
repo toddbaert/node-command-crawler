aa9d57bc7f786a3e7584ff9708f15989fa31253c
Baert <todd.baert@tag009442170705.dynatrace.org>
2018-05-17 09:33:21 -0400

    making path absolute

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var child = require("child_process");
 var targetDirectory = process.argv[2];
 var files = fs.readdirSync(targetDirectory);
 files.forEach(function (file) {
-    var tempFile = file.replace(/(\.[\w\d_-]+)$/i, '_temp$1');
-    child.execSync(('ffmpeg -i %file% -c:v copy -c:a aac -vbr 3 ' + tempFile).replace(/%file%/g, file));
-    child.execSync(('mv %file%.processing ' + tempFile).replace(/%file%/g, file));
+    if (file.indexOf('.mkv') > -1) {
+        var tempFile = file.replace(/(\.[\w\d_-]+)$/i, '_temp$1');
+        child.execSync(('ffmpeg -i %file% -c:v copy -c:a aac -vbr 3 ' + targetDirectory + '/' + tempFile).replace(/%file%/g, targetDirectory + '/' + file));
+        child.execSync(('mv %file%.processing ' + targetDirectory + '/' + tempFile).replace(/%file%/g, targetDirectory + '/' + file));
+    }
 });
--- app.ts
+++ app.ts
@@ -6,8 +6,10 @@ const targetDirectory : string = process.argv[2];
 
 var files: string[] = fs.readdirSync(targetDirectory);
 files.forEach(file => {
-    var tempFile: string = file.replace(/(\.[\w\d_-]+)$/i, '_temp$1');
-    child.execSync(('ffmpeg -i %file% -c:v copy -c:a aac -vbr 3 ' + tempFile).replace(/%file%/g, file));
-    child.execSync(('mv %file%.processing ' + tempFile).replace(/%file%/g, file));
+    if (file.indexOf('.mkv') > -1) {
+        var tempFile: string = file.replace(/(\.[\w\d_-]+)$/i, '_temp$1');
+        child.execSync(('ffmpeg -i %file% -c:v copy -c:a aac -vbr 3 ' + targetDirectory + '/' + tempFile).replace(/%file%/g, targetDirectory + '/' + file));
+        child.execSync(('mv %file%.processing ' + targetDirectory + '/' + tempFile).replace(/%file%/g, targetDirectory + '/' + file));
+    }
 });
 
